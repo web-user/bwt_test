221bbac75299598a3f8f5528cf9c87e9c8595294
web-users <srounov@gmail.com>
2016-02-16 03:35:17 +0200

    controller

--- www/controller/controller.php
+++ www/controller/controller.php
@@ -5,8 +5,25 @@ session_start();
 // include model 
 require_once MODEL;
 
+// connection library functions
+require_once 'functions/functions.php';
+
 // obtain a dynamic part of the pattern
 $view = empty($_GET['view']) ? 'reg' : $_GET['view'];
+
+switch ($view) {
+
+	case('reg'):
+		// registration
+	break;
+	case('weather'):
+		// weather
+	break;
+	case('feedback'):
+		// feedback
+	break;
+
+}
 
 
 // // include views
